9da18c32af7f07ea99c153bb8cfb13e47d5561cc
vitorugo1 <hvitor79@gmail.com>
2025-11-03 20:42:07 -0300

    added CORS middleware, allowing reqs from local server

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
 from .routers import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Allowing reqs from local client
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # Quais origens são permitidas
+    allow_credentials=True,
+    allow_methods=["*"],              # Quais métodos (GET, POST, etc)
+    allow_headers=["*"],              # Quais headers
+)
 
 app.include_router(router)
